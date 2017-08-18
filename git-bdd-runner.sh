3d2bf7769cc76ac7a3c752138f23a0692d88512b
Alexandr Varenik <varenikx@gmail.com>
2017-08-18 18:22:53 +0300

    git bdd runner

---
+++ git-bdd-runner.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+ID=$(uuidgen)
+
+mkdir $ID
+
+function killContainer {
+  containerId=$(docker ps -aqf "name=$ID")
+  docker kill $containerId
+}
+
+cd $HOME
+
+
+
+
+COMMAND="yarn run test:spec -- --filter=sales-pre --skipMenu"
+
+docker run --name "$ID" -e ID="$ID" -e GIT="$1" -e RUN="$COMMAND" -v "$HOME/$ID/":"/$ID" varenikx/chrome-bdd:latest &
+
+# 90 minutes
+for i in $(seq 1 60)
+  do
+    #slep
+    sleep 90
+
+    if [ -f "$HOME/$ID/1" ]; then
+      # failed
+      # for jenkins failed
+      killContainer
+      sleep 10
+      exit 1
+    fi
+
+    if [ -f "$HOME/$ID/0" ]; then
+      # ok
+      killContainer
+      sleep 10
+      exit 0
+    fi
+
+    if (( $i == 59 )); then
+      killContainer
+      sleep 10
+      echo -e "\x1b[5;41;37mFailed TIMEOUT\x1b[0m"
+      exit 1
+    fi
+done
