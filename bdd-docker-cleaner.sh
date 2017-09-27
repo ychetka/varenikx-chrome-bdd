1db75a9523d632541869ff084753a76ca9a0da58
Alexandr Varenik <varenikx@gmail.com>
2017-09-27 19:30:57 +0300

    remove  sudo

--- bdd-docker-cleaner.sh
+++ bdd-docker-cleaner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker rm -v $(docker ps -a -q -f status=exited)
 docker rmi $(docker images -f "dangling=true" -q)
-sudo -E -i -u root docker run -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker --rm martin/docker-cleanup-volumes
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker --rm martin/docker-cleanup-volumes
