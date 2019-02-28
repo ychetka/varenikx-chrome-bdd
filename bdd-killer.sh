fa0e0037af5c82015f2e7434d64c031dbbbec8a7
varenikx <varenikx@gmail.com>
2019-02-28 21:13:24 +0200

    fix killer

--- bdd-killer.sh
+++ bdd-killer.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-KILLER_LOG_FILE=$1
+KILLER_LOG_FILE=
+
+if [ -n "${1}" ]; then
+  KILLER_LOG_FILE=$1
+else
+  KILLER_LOG_FILE="/dev/null"
+fi
 
 NODE_PIDS=$(pgrep node)
 SOCAT_PIDS=$(pgrep socat)
 
 echo "START KILL..." > ${KILLER_LOG_FILE}
 
-for PID in ${NODE_PIDS}
+for NPID in ${NODE_PIDS}
 do
-    kill ${PID} >> ${KILLER_LOG_FILE} &
+    echo "KILL ${NPID}" >> ${KILLER_LOG_FILE}
+    kill ${NPID} >> ${KILLER_LOG_FILE} &
 done
 
-for PID in ${SOCAT_PIDS}
+for SPID in ${SOCAT_PIDS}
 do
-    kill ${PID} >> ${KILLER_LOG_FILE} &
+    echo "KILL ${SPID}" >> ${KILLER_LOG_FILE}
+    kill ${SPID} >> ${KILLER_LOG_FILE} &
 done
+
+echo "KILL COMPLETED" >> ${KILLER_LOG_FILE}
 
 exit 0
